70606853e81fecac8c243b6c30751b507ba4ea85
boj <bojieli@gmail.com>
2012-03-10 13:16:00 +0800

    add config

---
+++ config/config_db.php
@@ -0,0 +1,9 @@
+<?php
+// dababase configurations
+$_config_db['dbhost']  		= 'localhost';
+$_config_db['dbuser']  		= 'gewu-mysql';
+$_config_db['dbpw'] 	 	= 'bojdewangsite';
+$_config_db['dbcharset'] 	= 'utf8';
+$_config_db['pconnect'] 	= '0';
+$_config_db['dbname']  		= 'gewu';
+?>
