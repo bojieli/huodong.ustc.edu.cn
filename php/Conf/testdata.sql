83f69cdbac525648c94ac6831f1c0310545c3cdb
Bojie Li <bojieli@gmail.com>
2012-09-03 19:32:38 +0800

    [sql] fix testdata integrity

--- php/Conf/testdata.sql
+++ php/Conf/testdata.sql
@@ -1,6 +1,8 @@
 /*!40101 SET NAMES utf8 */;
 USE campus;
 
+INSERT INTO ustc_school SET name='中国科学技术大学';
+
 INSERT INTO ustc_user SET realname='李博杰', login_type=0, email='boj@mail.ustc.edu.cn', password=MD5(CONCAT(MD5('123456'),'salt')), salt='salt', status='active', isdeveloper=true, isschooladm=true, sid=1, student_no='PB10000603', gender=true, dept='少年班学院', major='计算机科学与技术', notify_email='bojieli@gmail.com', grade=2010, register_time=NOW(), hobby='', homepage='http://gewu.ustc.edu.cn/';
 INSERT INTO ustc_user SET realname='马甲', login_type=0, email='boj@mail.ustc.edu.cn', password=MD5(CONCAT(MD5('123456'),'salt')), salt='salt', status='active', isdeveloper=true, isschooladm=true, sid=1, student_no='PB10000603', gender=true, dept='少年班学院', major='计算机科学与技术', notify_email='bojieli@gmail.com', grade=2010, register_time=NOW(), hobby='', homepage='http://gewu.ustc.edu.cn/';
 INSERT INTO ustc_user SET realname='马甲2', login_type=0, email='boj@mail.ustc.edu.cn', password=MD5(CONCAT(MD5('123456'),'salt')), salt='salt', status='active', isdeveloper=true, isschooladm=true, sid=1, student_no='PB10000603', gender=true, dept='少年班学院', major='计算机科学与技术', notify_email='bojieli@gmail.com', grade=2010, register_time=NOW(), hobby='', homepage='http://gewu.ustc.edu.cn/';
