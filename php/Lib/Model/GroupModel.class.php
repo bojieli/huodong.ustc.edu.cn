b8bea42a014c41278ce220ffdf6a8fa44b23fdcd
Bojie Li <bojieli@gmail.com>
2012-09-04 21:28:37 +0800

    [clean] GroupModel

--- php/Lib/Model/GroupModel.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-class GroupModel extends Model {
-	public function schoolName($gid) {
-		$group = $this->find($gid);
-		$school = M('school')->field('name')->find($group['sid']);
-		return $school['name'];
-	}
-}
