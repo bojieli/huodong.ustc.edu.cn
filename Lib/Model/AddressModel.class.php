9e146bc5faba508e5a2d514799c8c3243c2d1915
kkwang <kkwang@126.com>
2012-09-18 22:02:47 +0800

    AddressModel.class.php

--- Lib/Model/AddressModel.class.php
+++ Lib/Model/AddressModel.class.php
@@ -5,7 +5,9 @@ class AddressModel extends Model {
 	{
 		$cond=array('gid'=>$gid,'sid'=>$sid);
 		$members = M('User_group')->field('uid')->where($cond)->order('priv desc')->select();
+		echo M('User_group')->getLastSql();
 		$members_info = array();
+		dump($members);
 		foreach($members as $m_id)
 		{
 			 $info = $this->addressInfo($m_id);
