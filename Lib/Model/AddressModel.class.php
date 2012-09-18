7e5870e7894caee5f6e44d7ed99463ff16210a83
kkwang <kkwang@126.com>
2012-09-18 22:15:58 +0800

    AddressModel.class.php

--- Lib/Model/AddressModel.class.php
+++ Lib/Model/AddressModel.class.php
@@ -4,20 +4,21 @@ class AddressModel extends Model {
 	public function createAddress($gid,$sid)
 	{
 		$cond=array('gid'=>$gid,'sid'=>$sid);
-		$members = M('User_group')->field('uid')->where($cond)->order('priv asc')->select();
+		$members = M('User_group')->field('uid,title')->where($cond)->order('priv asc')->select();
 		echo M('User_group')->getLastSql();
 		$members_info = array();
 		dump($members);
 		foreach($members as $m_id)
 		{
 			 $info = $this->addressInfo($m_id[uid]);
+			 $info['title'] = $m_id[title];
 			 $members_info[] = $info;
 		}
 		dump($members_info);
 	}
 	public function addressInfo($uid)
 	{
-		$info = M('User')->find($uid);
+		$info = M('User')->field('uid,sid,realname,email,grade,education,student_no,telephone,homepage,avatar')->find($uid);
 		return $info;
 	}
 }
