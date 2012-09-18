8fa021ce86a9dbb5224f070f0093cb8abb90233f
kkwang <kkwang@126.com>
2012-09-18 21:45:34 +0800

    add AddressModel.class.php

---
+++ Lib/Model/AddressModel.class.php
@@ -0,0 +1,21 @@
+<?php
+class AddressModel extends Model {
+	public function createAddress($gid,$sid)
+	{
+		$cond=array('gid'=>$gid,'sid'=>$sid);
+		$members = M('User_group')->field('uid')->where($cond)->order('priv desc')->select();
+		$members_info = array();
+		foreach($members as $m_id)
+		{
+			 $info = $this->addressInfo($m_id);
+			 $members_info = $info;
+		}
+		dump($members_info);
+	}
+	public function addressInfo($uid)
+	{
+		$info = M('User')->find($uid);
+		return $info;
+	}
+}
+?>
