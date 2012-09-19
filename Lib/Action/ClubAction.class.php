29d8509c70873f004f639a8552eb4c8b540cfea9
kkwang <kkwang@mail.ustc.edu.cn>
2012-09-19 22:40:25 +0800

    add address_fetion

--- Lib/Action/ClubAction.class.php
+++ Lib/Action/ClubAction.class.php
@@ -422,6 +422,42 @@ class ClubAction extends PublicAction {
 		$this->assign("members", $members);
 		$this->display();
 	}
+	public function createAddressFetion()
+	{
+		global $_G;
+		$gid = $this->getInputGid();
+		$sid = 1;
+		if(empty($_G[uid]))
+		{
+			$this->assign('jumpUrl','/User/login');
+			$this->error("您尚未登录");
+		}
+		if(!$this->isManager($gid)) {
+			$this->error("只有会长和部长才有权限查看通讯录");
+		}
+		$address = D('Address');
+		$members = $address->createAddress($gid,$sid);
+		$filename="./upload/address_fetion".$gid.".csv";
+		$file=fopen($filename,"w");
+		if($file){
+			fwrite($file,iconv( "UTF-8", "gbk" ,"姓名,手机,电子邮件"));
+			fwrite($file,"\r\n");
+			foreach($members as $key => $value)
+			{
+				$content = iconv( "UTF-8", "gbk" , "$value[realname],$value[telephone],$value[email]");
+				fwrite($file,"$content");
+				fwrite($file, " \r\n");
+			}
+		}
+		fclose($file);
+		header("Cache-Control:must-revalidate,post-check=0,pre-check=0");  
+		header("Content-Description:File Transfer");  
+		header ("Content-type: application/octet-stream"); //定义数据类型
+		header ("Content-Length: " . filesize ($filename));  
+		header("Content-Disposition: attachment; filename=".basename($filename)); 
+		readfile($filename);
+	}
+
 	public function createAddress()
 	{
 		global $_G;
