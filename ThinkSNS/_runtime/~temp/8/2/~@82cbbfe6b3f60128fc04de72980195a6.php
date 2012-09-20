b98d10c63007258cd2fa20e715124c586c8c9549
Bojie Li <bojieli@gmail.com>
2012-09-20 21:13:08 +0800

    chmod 755 => 644
    add ThinkSNS library

---
+++ ThinkSNS/_runtime/~temp/8/2/~@82cbbfe6b3f60128fc04de72980195a6.php
@@ -0,0 +1,3 @@
+<?php
+//000000000000a:19:{i:0;s:2:"id";i:1;s:3:"uid";i:2;s:5:"title";i:3;s:7:"explain";i:4;s:7:"contact";i:5;s:4:"type";i:6;s:5:"sTime";i:7;s:5:"eTime";i:8;s:7:"address";i:9;s:5:"cTime";i:10;s:8:"deadline";i:11;s:9:"joinCount";i:12;s:14:"attentionCount";i:13;s:10:"limitCount";i:14;s:12:"commentCount";i:15;s:7:"coverId";i:16;s:6:"optsId";s:8:"_autoinc";b:1;s:3:"_pk";s:2:"id";}
+?>
