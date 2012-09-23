401ff4a09bbcefc581f6ce22ef28c8cc2f4dece7
Bojie Li <bojieli@gmail.com>
2012-09-23 13:19:37 +0800

    remove unused Model files

--- Lib/Model/ActCommentModel.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ActCommentModel extends ViewModel {
-	public $viewFields = array(
-		'act_comment' => array('id', 'uid', 'content', 'time', 'rate'),
-		'user' => array('username', '_on'=>'act_comment.uid=user.uid')
-	);
-}
--- Lib/Model/ActModel.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-class ActModel extends Model {
-	public function query($page) {
-	}
-
-	public function detail($act_id) {
-	}
-}
