127fa39fed946cec024c0fae69709243548735ef
kkwang <kkwwangcool@126.com>
2012-10-07 10:48:00 +0800

    a

--- Lib/Action/UserAction.class.php
+++ Lib/Action/UserAction.class.php
@@ -29,7 +29,7 @@ class UserAction extends PublicAction {
          {   
              $realip   =   $_SERVER["REMOTE_ADDR"];   
          }   
-		echo $realip;
+		echo $realip."***";
 		die;
 	}
 	public function login()
