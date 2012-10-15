cf89d7cacddff04c3c967702c22db72d4ce0d99d
unknown <scgy@dell380_22.(none)>
2012-10-15 13:43:42 +0800

    sms

---
+++ Lib/Action/SmsAction.class.php
@@ -0,0 +1,29 @@
+﻿<?php
+class SmsAction extends PublicAction {
+		public function index()
+		{
+			$this->display();
+		}
+		public function sent(){
+		   global $_G;
+		   $uid= $_G['uid'];
+		   $to_all = $this->_post('tid');
+		   $msg = $this->_post('s');
+		   $Model = D('Sms');
+		   if($to_all==''||$msg=='')return 0;
+		   //dump($to_all);
+		   foreach ($to_all as $row =>$value)
+		   {
+				echo $value;
+				//$Model->getUserMobile($field);
+				//dump($Model->getUserMobile($value));
+				$mobiles[$value]=$Model->getUserMobile($value);				
+		   }
+		   //dump($msg);
+		   //dump($mobiles);
+		   $re=$Model->sentSms($msg,$mobiles);
+		   $this->success(print_r($re));
+		}
+
+}
+?>
