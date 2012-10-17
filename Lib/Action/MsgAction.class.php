46898f4a10450019742db1e4fe641976ee5afe66
ltx <olinlinlino@gmail.com>
2012-10-17 20:24:32 +0800

    ltx

---
+++ Lib/Action/MsgAction.class.php
@@ -0,0 +1,42 @@
+<?php
+class MsgAction extends Action {
+
+	public function index() {
+		global $_G;
+		$_G['uid'];
+		D('Msg')->init();
+		//dump(D('User')->getInfo(CURRENT_USER));
+		$this->assign('user_info', D('User')->getInfo(CURRENT_USER));
+		$this->assign('_G', $_G);
+		$this->show();
+	}
+
+public function show() {
+	$model = D('Msg');
+	  global $_G;
+	  if(empty($_G))
+	  return 0;
+	$show=$model->showMsg();
+	$this->assign('my', $show);
+	//dump($show);
+	//$this->assign('from_me', $show[0]);
+	//$this->assign('to_me', $show[1]);
+	$this->display();
+	}
+	public function create(){
+   $model = D('Msg');
+   global $_G;
+   $_G['uid'];
+   $_G['timestamp']=time();
+   if(empty($_G))	
+   return 0;
+   $msg=$this->_post('s')?$this->_post('s'):$this->_get('s');
+   $sub=$this->_post('sub')?$this->_post('sub'):$this->_get('sub');
+   $to_uid=$this->_post('t')?$this->_post('t'):$this->_get('t');
+   //echo $msg;
+   //return 0;
+   $re=$model->sentMsg($_G['timestamp'],$msg,$to_uid,$sub);
+    $this->success('yeah!!!');
+	}
+ }
+ ?>
---
+++ Lib/Model/MsgModel.class.php
@@ -0,0 +1,38 @@
+<?php
+class MsgModel extends Model {	
+	function init() {
+		 define('SUCCESS', 1);
+		 define('FAILED', 0);
+		 global $_G;
+		 $_G['timestamp']=time();
+		}
+		 function showMsg()
+        {   
+            global $_G;
+            
+			$con['uid']=$_G[uid];
+            $con['to_uid']=$_G[uid];
+            $con['_logic']='OR';
+			$my_msg=$this->where($con)->select();
+			//$from_me=$this->where("uid=".$_G[uid])->select();
+			//$to_me=$this->where("to_uid=".$_G[uid])->select();
+		    return $my_msg;
+        }
+        function sentMsg($time,$msg,$to_uid,$sub){
+        global $_G;
+        $ip=get_client_ip();
+        $data=array(
+			'uid'=>$_G['uid'],
+			'time'=>$_G['timestamp'],
+			'msg'=>$msg,
+			'sub'=>$sub?$sub:'blank',
+			'to_uid'=>$to_uid?$to_uid:0,
+			'ip'=>$ip,
+			'status'=>1
+		);
+		
+		$this->data($data)->add();      
+        }
+ 
+}
+?>
