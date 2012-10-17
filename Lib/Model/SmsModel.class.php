18182a76c46c03303834b07a8bb76c276827b6c4
unknown <shu@Shuhua-B12.(none)>
2012-10-17 13:59:36 +0800

    test

--- Lib/Model/SmsModel.class.php
+++ Lib/Model/SmsModel.class.php
@@ -53,13 +53,13 @@ class SmsModel extends Model {
 	}
 	public function sms_md5($msg,$uid,$tids)
 	{
-		$md5=$uid.'+'.$tids.'+'.$msg;
+		$md5=md5($uid."\t".$tids."\t".$msg);
 		$data=array(
 			'uid'   =>$uid,
 			'msg'   =>$msg,
 			'tids'	=>$tids,
 			'time'  =>time(),
-			'md5'=>md5($md5);
+			'md5'=>$md5
 		);
 		M('sms_md5')->data($data)->add();
 	}
