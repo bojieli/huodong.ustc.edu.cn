8652c90a51ae98e0edc7d2084104242afdf713c6
Bojie Li <bojieli@gmail.com>
2012-10-26 17:02:38 +0800

    ThinkSNS

--- ThinkSNS/_runtime/~temp/5/f/~@5f58b95ce38627d98019f89224a778c4.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:11:{i:0;s:2:"id";i:1;s:4:"cost";i:2;s:11:"costExplain";i:3;s:8:"province";i:4;s:4:"city";i:5;s:4:"area";i:6;s:4:"opts";i:7;s:5:"isHot";i:8;s:5:"rTime";s:8:"_autoinc";b:1;s:3:"_pk";s:2:"id";}
-?>
