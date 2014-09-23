d3568f5176f3d70caeaef2b1c6a91a5d9c65394c
Taixing Lin <olinlinlino@gmail.com>
2014-09-23 09:43:44 +0800

    增加选项

--- Lib/Action/CrxAction.class.php
+++ Lib/Action/CrxAction.class.php
@@ -21,25 +21,31 @@ class CrxAction extends PublicAction{
         }
     }
     public function uploadAPK(){
+
 			$filename = $this->upload();
 			if (!$filename) {
 				$this->error("您必须上传APK格式文件！");
 			}
-			
-			$crx = $this->apk2crx($filename);
+			$type = $this->_post("type");
+			$crx = $this->apk2crx($filename,$type);
 			//var_dump($crx);die();
 			$url = "/upload/apk/".$crx;
 			$url =  "下载：<a style='border: 1px solid red;padding: 5px;' href=".$url .">".$crx."</a>";
 			$this->assign('url', $url);
 			$this->display();
 	}
-private function apk2crx($filename){
+private function apk2crx($filename,$type="phone"){
 	//echo $source;die();
 	if(!file_exists("./upload/apk/".$filename)){
 	    echo $filename . " does not exist <br />";
 	    return -1;
 	}
-	$cmd = "cd ./upload/apk/;/usr/local/bin/apk2crx " . $filename. " 2>&1";
+	if ($type=="phone") {
+		$cmd = "cd ./upload/apk/;/usr/local/bin/apk2crx " .$filename. " 2>&1";
+	}
+	else{
+		$cmd = "cd ./upload/apk/;/usr/local/bin/apk2crx " .$filename. " pad 2>&1";
+	}
 	//var_dump($filename);die();
 	return shell_exec($cmd);
 }
