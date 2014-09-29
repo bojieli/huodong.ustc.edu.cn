1862994051d3cba7dcc30fcfdc0d768f8fd60c5c
Taixing Lin <olinlinlino@gmail.com>
2014-09-29 18:12:58 +0800

    test

--- Common/filelist.php
+++ Common/filelist.php
@@ -305,8 +305,10 @@ function stateFormat($state) {
  * @return string HTML link
  */
 function linkToFile($full_path){
-	return '<a href="?download='. base64_encode($full_path) . '" target="_blank">'
-			. download . '</a>';
+	/*return '<a href="?download='. base64_encode($full_path) . '" target="_blank">'
+			. download . '</a>';*/
+	return '<a href="'.$full_path.'" target="_blank">'
+	. download . '</a>'
 }
 /**
  * Download file method
