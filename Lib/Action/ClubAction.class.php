6128b991733773a1a80649351a3c2218db71715a
Taixing Lin <olinlinlino@gmail.com>
2014-10-11 16:22:23 +0800

    社团会员管理增加搜索功能

--- Lib/Action/ClubAction.class.php
+++ Lib/Action/ClubAction.class.php
@@ -654,38 +654,26 @@ class ClubAction extends PublicAction {
     }
 
     public function manage() {
-        //import(“ORG.Util.Page”);// 导入分页类
+        import("ORG.Util.Page");// 导入分页类
 
         $gid = $this->getInputGid();
-        
-        $page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
-        //$start = isset($_GET['start']) && is_numeric($_GET['start']) ? $_GET['start'] : 0;
-        $num = isset($_GET['num']) && is_numeric($_GET['num']) ? $_GET['num'] : 20;
-        $start = ($page-1)*$num;
-        
-        $club = $this->getData($gid);
-        $this->assign('club', $club);
-        
-        if (!$this->isManager($gid)) {
-            $start = 0;
-            $num = 20;
+        $keyword = isset($_GET['keyword']) ? addslashes($_GET['keyword']) : '';
+
+           if(!empty($keyword) && $this->isManager($gid))
+        {
+            $search_condition = "and (realname like '%$keyword%' or email like '%$keyword%' or education like '%$keyword%'  or student_no like '%$keyword%'  or dept like '%$keyword%'  or telephone like '%$keyword%'  or title like '%$keyword%') ";    
         }
-        //$count = M()->query("SELECT count(*) FROM (ustc_user INNER JOIN ustc_user_group ON ustc_user.uid = ustc_user_group.uid) INNER JOIN ustc_priv ON ustc_user_group.priv = ustc_priv.priv_name WHERE ustc_user_group.gid='$gid'");
-        
-        //$Page = new Page($count,20);
-        $members = M()->query("SELECT * FROM (ustc_user INNER JOIN ustc_user_group ON ustc_user.uid = ustc_user_group.uid) INNER JOIN ustc_priv ON ustc_user_group.priv = ustc_priv.priv_name WHERE ustc_user_group.gid='$gid' ORDER BY ustc_priv.priv_value desc LIMIT $start,$num");
-       // $members = M()->query("SELECT * FROM (ustc_user INNER JOIN ustc_user_group ON ustc_user.uid = ustc_user_group.uid) INNER JOIN ustc_priv ON ustc_user_group.priv = ustc_priv.priv_name WHERE ustc_user_group.gid='$gid' ORDER BY ustc_priv.priv_value desc LIMIT $Page->firstRow,$Page-
-       // >listRows");
+        $count = M()->query("SELECT count(*) as count FROM (ustc_user INNER JOIN ustc_user_group ON ustc_user.uid = ustc_user_group.uid) INNER JOIN ustc_priv ON ustc_user_group.priv = ustc_priv.priv_name WHERE ustc_user_group.gid='$gid' $search_condition")  [0]["count"];
+       
+       if(!$this->isManager($gid)){
+            $count=10;
+        }
+        $Page = new Page($count,10);
+        $Page->setConfig('theme',"%upPage% %linkPage% %downPage%");
+        $show       = $Page->show();
+
+        $members = M()->query("SELECT * FROM (ustc_user INNER JOIN ustc_user_group ON ustc_user.uid = ustc_user_group.uid) INNER JOIN ustc_priv ON ustc_user_group.priv = ustc_priv.priv_name WHERE ustc_user_group.gid='$gid' $search_condition ORDER BY ustc_priv.priv_value desc LIMIT $Page->firstRow,$Page->listRows");
     
-       // if(count($member)>0)$count = count($member);
-
-       // $Page = new Page($count,20);
-
-        //$show = $Page->show();
-
-        //$this->assign('list',$list);// 赋值数据集
-        //$this->assign('members',$show);// 赋值分页输出
-        //$this->display(); // 输出模板
 
         foreach ($members as &$member) {
             $member['avatar'] = D('user')->getAvatar($member['uid'],'small');
@@ -711,16 +699,13 @@ class ClubAction extends PublicAction {
         $departments =M("Team")->where(array('type'=>'department','gid'=>$gid,'flag'=>1))->select();
 		
         $acts = D('Activity')->getActsByGid($gid);
-		
 
-        //dump($acts);
-		$this->assign('acts', $acts);
-		$this->assign('teams', $teams);
-		$this->assign('departments', $departments);
+        $this->assign('acts', $acts);
+        $this->assign('teams', $teams);
+        $this->assign('departments', $departments);
 
         $this->assign('inactive', $inactive_members);
-        $this->assign('pageStart', $start);
-        $this->assign('pageNow', $page);
+        $this->assign('page',$show);
         $this->headnav();
         $this->display();
     }
@@ -817,24 +802,7 @@ class ClubAction extends PublicAction {
         } else
             $this->error('修改title失败');
     } 
-    public function test()
-    {
-        $clubs = M('Club')->where("1")->select();
-        foreach($clubs as $value)
-        {
-			$num = M('User_group')->result_first("SELECT count(*) FROM ustc_user_group where gid = $value[gid] and sid = 1 and priv !='inactive'");
-			$record['member_count'] = $num;
-			M('Club')->where(['gid'=>$value[gid]])->save($record);
-			dump(M('Club')->getLastSql());
-        }
-		
-        /*M('Club')->query("update ustc_user_group set priv = 'vice-admin' where title = '副主席'");
-        M('Club')->query("update ustc_user_group set priv = 'vice-admin' where title = '副会长'");
-        M('Club')->query("update ustc_user_group set priv = 'vice-admin' where title = '副社长'");
-        M('Club')->query("update ustc_user_group set priv = 'vice-manager' where title = '副部长'");
-        M('Club')->query("update ustc_user_group set priv = 'team-leader' where title = '活动负责人'");
-        M('Club')->query("update ustc_user_group set priv = 'team-leader' where title = '项目组长'");*/
-    }
+    
 
     public function removeMember() {
         list($gid, $uid) = $this->getInputGidUid();
